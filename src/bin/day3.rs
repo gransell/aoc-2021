0816c5cb9526a1d4cd7e392ea60d83bfc0a5e998
Mikael Gransell <gransell@gmail.com>
2021-12-04 21:24:38 +0100

    Day 3

---
+++ src/bin/day3.rs
@@ -0,0 +1,92 @@
+use std::io::{self, BufRead};
+
+#[allow(dead_code)]
+fn part1(lines: &[String]) {
+  let length = lines.first().unwrap().len();
+  let gamma_string: String = (0..length)
+    .map(|idx| {
+      let most_common = lines
+        .iter()
+        .fold((0, 0), |acc, line| match line.chars().nth(idx) {
+          Some('0') => (acc.0 + 1, acc.1),
+          Some('1') => (acc.0, acc.1 + 1),
+          _ => panic!("Invalid character"),
+        });
+      if most_common.0 > most_common.1 {
+        "0".to_owned()
+      } else {
+        "1".to_owned()
+      }
+    })
+    .collect();
+  let gamma = i32::from_str_radix(&gamma_string, 2).unwrap();
+  let epsilon = !gamma;
+
+  println!("Power: {}", gamma * epsilon);
+}
+
+fn generate<F>(lines: &Vec<String>, f: F) -> i32
+where
+  F: Fn(i32, i32) -> bool,
+{
+  let length = lines.first().unwrap().len();
+  let string = (0..length).fold(lines.clone(), |acc, idx| {
+    if acc.len() == 1 {
+      return acc;
+    }
+    let num_bits = acc
+      .iter()
+      .fold((0, 0), |acc, line| match line.chars().nth(idx) {
+        Some('0') => (acc.0 + 1, acc.1),
+        Some('1') => (acc.0, acc.1 + 1),
+        _ => panic!("Invalid character"),
+      });
+    let f_res = f(num_bits.0, num_bits.1);
+    let to_keep = if f_res { '0' } else { '1' };
+    let result = acc
+      .iter()
+      .filter(|line| line.chars().nth(idx) == Some(to_keep))
+      .map(|line| line.to_owned())
+      .collect();
+    // println!("{:?}", result);
+    return result;
+  });
+  i32::from_str_radix(string.first().unwrap(), 2).unwrap()
+}
+
+fn part2(lines: &Vec<String>) {
+  let oxygen = generate(lines, |a, b| a > b);
+  let co2 = generate(lines, |a, b| a <= b);
+  println!("Oxygen: {}", oxygen);
+  println!("CO2: {}", co2);
+  println!("Life support rating: {}", oxygen * co2);
+}
+
+#[test]
+fn part_2() {
+  let lines = vec![
+    "00100".to_owned(),
+    "11110".to_owned(),
+    "10110".to_owned(),
+    "10111".to_owned(),
+    "10101".to_owned(),
+    "01111".to_owned(),
+    "00111".to_owned(),
+    "11100".to_owned(),
+    "10000".to_owned(),
+    "11001".to_owned(),
+    "00010".to_owned(),
+    "01010".to_owned(),
+  ];
+  part2(&lines);
+}
+
+fn main() {
+  let lines: Vec<String> = io::stdin()
+    .lock()
+    .lines()
+    .map(|line| line.unwrap())
+    .collect();
+
+  part2(&lines);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,185 @@
 pub mod parse {
-  pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
-    move |input| match input.get(0..expected.len()) {
-      Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+
+  type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+  pub trait Parser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+  }
+
+  impl<'a, F, Output> Parser<'a, Output> for F
+  where
+    F: Fn(&'a str) -> ParseResult<Output>,
+  {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+      self(input)
+    }
+  }
+
+  struct BoxedParser<'a, Output> {
+    parser: Box<dyn Parser<'a, Output> + 'a>,
+  }
+
+  impl<'a, Output> BoxedParser<'a, Output> {
+    fn new<P>(parser: P) -> Self
+    where
+      P: Parser<'a, Output> + 'a,
+    {
+      BoxedParser {
+        parser: Box::new(parser),
+      }
+    }
+  }
+
+  impl<'a, Output> Parser<'a, Output> for BoxedParser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+      self.parser.parse(input)
+    }
+  }
+
+  pub fn match_literal<'a>(expected: &'static str) -> impl Parser<'a, ()> {
+    move |input: &'a str| {
+      if let Some(stripped) = input.strip_prefix(expected) {
+        Ok((stripped, ()))
+      } else {
+        Err(input)
+      }
+    }
+  }
+
+  pub fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
+  where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+  {
+    move |input| {
+      parser1.parse(input).and_then(|(next_input, result1)| {
+        parser2
+          .parse(next_input)
+          .map(|(last_input, result2)| (last_input, (result1, result2)))
+      })
+    }
+  }
+
+  pub fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
+  where
+    P: Parser<'a, A>,
+    F: Fn(A) -> B,
+  {
+    move |input| {
+      parser
+        .parse(input)
+        .map(|(next_input, result)| (next_input, map_fn(result)))
+    }
+  }
+
+  pub fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+  where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+  {
+    map(pair(parser1, parser2), |(left, _right)| left)
+  }
+
+  pub fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+  where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+  {
+    map(pair(parser1, parser2), |(_left, right)| right)
+  }
+
+  pub fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+  where
+    P: Parser<'a, A>,
+  {
+    move |mut input| {
+      let mut result = Vec::new();
+
+      if let Ok((next_input, first_item)) = parser.parse(input) {
+        input = next_input;
+        result.push(first_item);
+      } else {
+        return Err(input);
+      }
+
+      while let Ok((next_input, next_item)) = parser.parse(input) {
+        input = next_input;
+        result.push(next_item);
+      }
+
+      Ok((input, result))
+    }
+  }
+
+  pub fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+  where
+    P: Parser<'a, A>,
+  {
+    move |mut input| {
+      let mut result = Vec::new();
+
+      while let Ok((next_input, next_item)) = parser.parse(input) {
+        input = next_input;
+        result.push(next_item);
+      }
+
+      Ok((input, result))
+    }
+  }
+
+  pub fn any_char(input: &str) -> ParseResult<char> {
+    match input.chars().next() {
+      Some(next) => Ok((&input[next.len_utf8()..], next)),
       _ => Err(input),
     }
   }
 
-  #[test]
-  fn literal_parser() {
-    let parse_joe = match_literal("Hello Joe!");
-    assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
-    assert_eq!(
-      Ok((" Hello Robert!", ())),
-      parse_joe("Hello Joe! Hello Robert!")
-    );
-    assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+  pub fn pred<'a, P, A, F>(parser: P, predicate: F) -> impl Parser<'a, A>
+  where
+    P: Parser<'a, A>,
+    F: Fn(&A) -> bool,
+  {
+    move |input| {
+      if let Ok((next_input, value)) = parser.parse(input) {
+        if predicate(&value) {
+          return Ok((next_input, value));
+        }
+      }
+      Err(input)
+    }
   }
 
-  fn identifier(input: &str) -> Result<(&str, String), &str> {
-    let mut matched = String::new();
-    let mut chars = input.chars();
+  #[test]
+  fn test_literal_parser() {
+    let parse_joe = match_literal("Hello Joe!");
+    assert_eq!(Ok(("", ())), parse_joe.parse("Hello Joe!"));
+    assert_eq!(
+      Ok((" Hello Robert!", ())),
+      parse_joe.parse("Hello Joe! Hello Robert!")
+    );
+    assert_eq!(Err("Hello Mike!"), parse_joe.parse("Hello Mike!"));
+  }
 
-    match chars.next() {
-      Some(next) if next.is_alphabetic() => matched.push(next),
-      _ => return Err(input),
-    }
+  #[test]
+  fn one_or_more_combinator() {
+    let parser = one_or_more(match_literal("ha"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("hahaha"));
+    assert_eq!(Err("ahah"), parser.parse("ahah"));
+    assert_eq!(Err(""), parser.parse(""));
+  }
 
-    while let Some(next) = chars.next() {
-      if next.is_alphanumeric() || next == '-' {
-        matched.push(next);
-      } else {
-        break;
-      }
-    }
+  #[test]
+  fn zero_or_more_combinator() {
+    let parser = zero_or_more(match_literal("ha"));
+    assert_eq!(Ok(("", vec![(), (), ()])), parser.parse("hahaha"));
+    assert_eq!(Ok(("ahah", vec![])), parser.parse("ahah"));
+    assert_eq!(Ok(("", vec![])), parser.parse(""));
+  }
 
-    let next_index = matched.len();
-    Ok((&input[next_index..], matched))
+  #[test]
+  fn predicate_combinator() {
+    let parser = pred(any_char, |c| *c == 'o');
+    assert_eq!(Ok(("mg", 'o')), parser.parse("omg"));
+    assert_eq!(Err("lol"), parser.parse("lol"));
   }
 }
